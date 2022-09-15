3fea22cd11c9305c82539655c4408f1e7bab7f8b
Eddy Cabrera <eddalexcab@gmail.com>
2022-09-15 00:41:23 -0600

    Se agrego el DbAccess, para probarlo se agrego un modelo y un controller con dos views

---
+++ ModuloInventarioWeb/Data/CategoryData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ModuloInventarioWeb.DbAccess;
+using ModuloInventarioWeb.Models;
+
+namespace ModuloInventarioWeb.Data;
+
+public class CategoryData : ICategoryData
+{
+    private readonly ISqlDataAccess _db;
+
+    public CategoryData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<Category>> GetCategories()
+    {
+        return _db.LoadData<Category, dynamic>("SPCategory_GetAll", new { });
+    }
+
+    //public Task<IEnumerable<Category>> GetCategories() => _db.LoadData<Category, dynamic>("SPCategory_GetAll", new { });
+
+    public async Task<Category?> GetCategory(int id)
+    {
+        var results = await _db.LoadData<Category, dynamic>("SPCategory_GetById", new { id });
+
+        return results.FirstOrDefault();
+    }
+}
+
---
+++ ModuloInventarioWeb/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace ModuloInventarioWeb.DbAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "DefaultConnectionString");
+        Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "DefaultConnectionString");
+    }
+}
