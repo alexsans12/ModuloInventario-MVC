be348db8ed03b3efa011b9ac604ac11cb0568f0a
Jorge Barillas <jjbarillasparedes123@gmail.com>
2022-10-25 23:33:28 -0600

    Terminado

--- ModuloInventarioWeb/Models/Usuario.cs
+++ ModuloInventarioWeb/Models/Usuario.cs
@@ -6,8 +6,8 @@ public class Usuario
     public int ID_Usuario { get; set; }
     public string Nombre { get; set; }
     public string Contrasena { get; set; }
-    public byte[] Foto_Perfil { get; set; }
-    public int ID_Rol { get; set; }
+    public byte[]? Foto_Perfil { get; set; }
+
     public int ID_Empleado { get; set; }
 
 }
